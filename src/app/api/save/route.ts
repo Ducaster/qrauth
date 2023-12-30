d972915cc62986dbb08ee9cf015b62b7e33eff1e
Higashino-Keigo <kooa3399@kakao.com>
2023-12-30 18:20:47 +0900

    changetitle

--- src/app/api/save/route.ts
+++ src/app/api/save/route.ts
@@ -2,6 +2,7 @@ import { NextRequest, NextResponse } from 'next/server';
 import { GoogleSpreadsheet } from 'google-spreadsheet';
 import { JWT } from "google-auth-library";
 
+// 구글 문서를 불러오는 함수
 async function loadGoogleDoc(){
   try{
     const key = process.env.REACT_APP_GOOGLE_PRIVATE_KEY;
@@ -25,10 +26,13 @@ export async function POST(
   req: NextRequest,
 ){
   try{
+    // 구글 문서를 불러옴
     const doc = await loadGoogleDoc();
     if(!doc){
       return NextResponse.json({error: "Internal Server Error(load Doc)"}, {status: 500});
     }
+
+    // sheet1이 있는지 확인하고, 없다면 생성
     let sheet = doc.sheetsByTitle["sheet1"];
     if(!sheet){
         console.log('Create a new sheet');
@@ -38,12 +42,14 @@ export async function POST(
         });
     }
 
+    // NextRequest로부터 데이터를 가져옴
     const body = req.body;
     if (!body) {
         console.log('Body is null');
         return NextResponse.json({erorr: "Bad Request"},{status: 400});
     };
 
+    // 데이터를 텍스트로 변환
     const processText = async () => {
         const reader = body.getReader();
         let result = '';
@@ -55,10 +61,12 @@ export async function POST(
       
         return result;
       };
-      
+    // 텍스트를 정리하고 json형식으로 변환
     const result = await processText();
     const cleanedBody = result.replace(/\\/g, '').slice(11, -2); 
     const jsonData = JSON.parse(cleanedBody);
+
+    // 변환한 json형식대로 sheet에 추가
     await sheet.addRow({
         이름: jsonData.name,
         지역: jsonData.region,
